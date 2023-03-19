5ab2979537494e46b09f3fed610559c178983288
Gabriel Gutierrez <97479321+GaboProgramers@users.noreply.github.com>
2023-03-19 11:59:47 -0500

    agregando rutas
    las rutas publications-type, states y roles son agregadas

--- routes/models.router.js
+++ routes/models.router.js
@@ -4,12 +4,18 @@ const express = require('express')
 // const isAuthenticatedByPassportJwt = require('../libs/passport')
 
 const routesAuth = require('./auth.routes')
+const routerPublicationsType = require('./publicationsTypes.routes')
+const routerStates = require('./states.routes')
+const routerRoles = require('./roles.routes')
 
 function routerModels(app) {
   const router = express.Router()
 
   app.use('/api/v1', router)
   router.use('/auth', routesAuth)
+  router.use('/publications-types', routerPublicationsType)
+  router.use('/states', routerStates)
+  router.use('/roles', routerRoles)
 }
 
 module.exports = routerModels
